6146c4ba3b59e54e2bd507603651700463dd5c7b
Leonardo Martínez Núñez <lemanusoft@hotmail.com>
2025-06-25 16:17:10 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace manufacturin_solution_apis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
